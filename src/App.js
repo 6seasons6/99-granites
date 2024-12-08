156946d59d0e61528686c534374fb16f1a8e2ab1
Ananyapanyala <ananya.panyala@gmail.com>
2024-12-08 16:23:04 +0530

    navbar as component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
+// src/App.js
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'; // Import Navbar component
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar /> {/* Include Navbar component here */}
+      {/* You can add other components or content here */}
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'; // Import from react-dom/client
+import './index.css'; // Your styles if any
+import App from './App'; // Importing the main App component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root using createRoot
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
